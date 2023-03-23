c79d9cee3f494985f4efac46304f6a9d070809bd
abasha <abasha@newrelic.com>
2023-03-23 14:50:12 +0530

    remove headless from config

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,6 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    headless: false,
     testIdAttribute: 'data-test-id',
   },
 
