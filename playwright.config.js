fbdc50e18e9064ca2cfacb1df82496fee0d39f95
abasha1234 <127721368+abasha1234@users.noreply.github.com>
2023-03-24 10:41:30 +0530

    update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,6 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     testIdAttribute: 'data-test-id',
-    headless: false,
   },
 
   /* Configure projects for major browsers */
