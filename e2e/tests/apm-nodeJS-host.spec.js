28b95031c8ef1baa3d19f23538c4fd4690d0d86d
Julien Erard <jerard@newrelic.com>
2023-06-28 17:14:19 -0700

    fix: installation and poll status with dynamo

--- e2e/tests/apm-nodeJS-host.spec.js
+++ e2e/tests/apm-nodeJS-host.spec.js
@@ -1,11 +1,13 @@
 import { test, expect } from "@playwright/test";
 import deployConfig from './apm-nodeJS-host-deploy.json';
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+import { DynamoDBClient, BatchGetItemCommand } from "@aws-sdk/client-dynamodb";
+
 const { 
   v4: uuidv4,
 } = require('uuid');
 
-var AWS = require('aws-sdk');
-AWS.config.update({region: process.env.AWS_REGION});
+const awsConfig = {region: process.env.AWS_REGION}
 
 const { chromium } = require("playwright");
 
@@ -34,6 +36,14 @@ test.afterAll(async () => {
 test.describe("Node JS host installation", () => {
   test("should install on a newly created host", async () => {
     test.slow();
+    test.setTimeout(600000); // 10min
+
+    let installCtx = {
+      done: false,
+      messageId: undefined,
+      item: undefined,
+      appName: `nodejstest-${Number(new Date())}`
+    };
 
     await page.getByText("Add data").first().click();
 
@@ -41,22 +51,15 @@ test.describe("Node JS host installation", () => {
 
     await page.getByText("On a host").first().click();
 
-    // Fill app name
-    // const applicationNameContainer = await page.locator('#text-field-2');
-    // const applicationNameInput =
-    // applicationNameContainer.locator('input[type="text"]');
-    // *[@id="shared-component-installator.node-js-installation"]/div/div/section
-
     const container = await page.locator(
       'div[id="shared-component-installator.node-js-installation"]'
     );
 
+    // Set App Name
     const applicationNameInput = await container.locator(
       'input[type="text"]'
     );
-    const appName = "nodetestapp1"
-    await applicationNameInput.fill(appName);
-
+    await applicationNameInput.fill(installCtx.appName);
     await page.getByText("Save").first().click();
 
     // step 1 - copy install newrelic
@@ -69,37 +72,121 @@ test.describe("Node JS host installation", () => {
         break
       }
     }
+
+    /////////////////////////////
+    // Send an installation event
     const licenseKey = ""
-    const envVars = `NEW_RELIC_LICENSE_KEY=${licenseKey} NEW_RELIC_APP_NAME=${appName}`
+    const envVars = `NEW_RELIC_LICENSE_KEY=${licenseKey} NEW_RELIC_APP_NAME=${installCtx.appName}`
     const stopNodetron = "sudo /usr/bin/supervisorctl stop nodetron1"
 
     deployConfig.instrumentations.services[0].params.step_configure = command[0]
-    deployConfig.instrumentations.services[0].params.step_onafterstart = `${stopNodetron};${envVars} ${command[1].replace("YOUR_MAIN_FILENAME.js","server.js")} config/app_config.json 2>&1 &`
-
+    deployConfig.instrumentations.services[0].params.step_onafterstart = `sudo ${stopNodetron};${envVars} ${command[1].replace("YOUR_MAIN_FILENAME.js","server.js")} config/app_config.json 2>&1 &`
     // console.log(deployConfig)
 
-    // Create an SQS service object
-    var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+    var messageDeduplicationId = uuidv4();
+    const client = new SQSClient(awsConfig);
+    const input = { // SendMessageRequest
+      QueueUrl: "https://sqs.us-east-2.amazonaws.com/709144918866/testDeployerQueue.fifo",
+      MessageBody: JSON.stringify(deployConfig,null,2),
+      MessageAttributes: { },
+      MessageDeduplicationId: messageDeduplicationId,
+      MessageGroupId: messageDeduplicationId,
+    };
+    const sqsCommand = new SendMessageCommand(input);
+    const response = await client.send(sqsCommand);
+    installCtx.messageId = response.MessageId;
+    console.log("Success SQS messageId:", installCtx.messageId);
+    // installCtx.messageId = "ddb7dce7-3a63-4243-951b-69eb0f675062"
+    // installCtx.messageId = "invalid one"
 
-    const messageId = uuidv4();
-    var params = {
-     MessageAttributes: {
-      
-     },
-     MessageBody: JSON.stringify(deployConfig,null,2),
-     MessageDeduplicationId: messageId,
-     MessageGroupId: messageId,
-     QueueUrl: "https://sqs.us-east-2.amazonaws.com/709144918866/testDeployerQueue.fifo"
-   };
+    await page.getByRole("button", { name: "Continue" }).click();
 
-   sqs.sendMessage(params, function(err, data) {
-    if (err) {
-      console.log("Error", err);
-      expect(true).toBeFalsy();
-    } else {
-      console.log("Success", data.MessageId);
+    await expect(
+      page.getByText("Connect your Logs and Infrastructure")
+    ).toBeVisible();
+    
+    await page.getByRole('radio', { name: 'I want to install the agent directly on my host' }).click();
+
+    await page.getByRole('radio', { name: 'Linux' }).click();
+
+    await page.getByLabel('Automatically answer "yes" to all install prompts. We\'ll stop the installer if there\'s an error.').check();
+
+    const infraStepCopy = await page.locator("div.TerminalNode-terminal")
+    var command = []
+    for (const item of await infraStepCopy.all()) {
+      const val = (await item.innerText())
+      command.push(`${val}`)
+      if (command.length == 2) {
+        break
+      }
     }
-  });
+    
+    await page.getByRole("button", { name: "Continue" }).click();
+
+    async function waitForInstallationComplete(installCtx) {
+      const input = {
+        "RequestItems": {
+          "DeployResponse": {
+            "Keys": [
+              {
+                "MessageId": {S: installCtx.messageId }
+              },
+            ]
+          }
+        }
+      };
+      const command = new BatchGetItemCommand(input);
+      const client = new DynamoDBClient(awsConfig);
+      let maxTimeoutSeconds = 600   // 10min
+      let delayTimeMilliSeconds = 10000 // 10sec
+      let maxTimeoutDate = (new Date() / 1000)+maxTimeoutSeconds
+      while ( (!installCtx.done) && (new Date() / 1000) < maxTimeoutDate) {
+        const response = await client.send(command);
+        if (response.Responses && response.Responses.DeployResponse && response.Responses.DeployResponse.length > 0) {
+          var item = response.Responses.DeployResponse[0]
+          installCtx.done = true;
+          installCtx.item = item;
+          if (item.Code && item.Code.N == "0") {
+            console.log("Installation Success")
+            return Promise.resolve()
+          } else {
+            console.log("Installation Failed")
+            expect(true).toBeFalsy();
+            return Promise.reject();
+          }
+        }
+        console.log("Installation not completed yet, will retry")
+        await new Promise(r => setTimeout(r, delayTimeMilliSeconds));
+      }
+      if (!installCtx.done) {
+        console.log("Installation Failed Timeout, result:", installCtx.Item);
+        expect(true).toBeFalsy();
+        return Promise.reject();
+      }
+    }
+
+    await waitForInstallationComplete(installCtx);
+
+    console.log("Testing connection now")
+    await page.getByRole('button', { name: 'Test connection' }).click();
+
+    await expect(
+      page.locator('div').filter({hasText: /^Node\.js agent$/}).first()
+    ).toBeVisible();
+
+    await expect(
+      page.locator('div').filter({hasText: /^On-host logs$/}).first()
+    ).toBeVisible();
+
+    await expect(
+      page.locator('div').filter({hasText: /^Infrastructure agent$/}).first()
+    ).toBeVisible();
+
+    await expect(
+      await page.getByText("Successfully installed.")
+    ).toBeVisible();
+
+    // await page.getByRole('link', {name: 'See your data'}).click();
 
     await page.waitForLoadState("networkidle");
 
